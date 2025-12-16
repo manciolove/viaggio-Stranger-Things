882cb5f1f99a85637f80c1d604966d833eddc114
manciolove <114284283+manciolove@users.noreply.github.com>
2025-12-16 10:36:53 +0100

    modifica creatura

--- viaggio-Stranger-Things/Program.cs
+++ viaggio-Stranger-Things/Program.cs
@@ -258,7 +258,6 @@ namespace gioco_stranger_things
                 Console.WriteLine("##                   Vecna                          ##");
                 Console.WriteLine("######################################################");
                 Console.WriteLine("");
-                Console.WriteLine("string creature = @\"");
                 Console.WriteLine("            .|||||||:::.         ");
                 Console.WriteLine("         /|||||||||||||||\\       ");
                 Console.WriteLine("       / /||@ @ @ @ @||\\ \\      ");
@@ -276,7 +275,6 @@ namespace gioco_stranger_things
                 Console.WriteLine("       // /| | | | | |\\ \\\\       ");
                 Console.WriteLine("      // /  | | | | |  \\ \\\\      ");
                 Console.WriteLine("\";");
-                Console.WriteLine("Console.WriteLine(creature);");
                 Console.WriteLine("");
                 Console.WriteLine("######################################################");
                 //condizione vittoria contro Vecna
